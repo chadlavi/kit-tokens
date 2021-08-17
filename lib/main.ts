44243bfb79853380635a2d7341eddd236f35f9fc
Chad Lavimoniere <chad@chadlavimoniere.com>
2021-08-16 20:32:20 -0400

    add text tokens

--- lib/main.ts
+++ lib/main.ts
@@ -1,13 +1,17 @@
 import { color } from "./color";
 import { spacing } from "./spacing";
+import { text } from "./text";
 
 const theme = {
   color,
-  spacing
+  spacing,
+  text
 };
 
 export { color };
 
 export { spacing };
+
+export { text };
 
 export default theme;
